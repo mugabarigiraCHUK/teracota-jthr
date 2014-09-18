bd57ccbd5a997ebd857d124c25f7c6f7ffb1a466
gazsia <gazsia@gmail.com>
2014-09-18 17:15:25 +0300

    Created HomeServlet Changed login.jsp
    Started working on LoginServlet
            -> login not working, probably web.xml is badly configured

--- jthr/jthr-parent/jthr-web/src/main/java/edu/msg/jthr/web/login/LoginServlet.java
+++ jthr/jthr-parent/jthr-web/src/main/java/edu/msg/jthr/web/login/LoginServlet.java
@@ -1,11 +1,15 @@
 package edu.msg.jthr.web.login;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
@@ -13,27 +17,49 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		Object loginObject = request.getSession().getAttribute("loggedIn");
+		if (loginObject != null && loginObject.equals(false)) {
+			response.getWriter().println("Username or passwrod incorrect");
+		}
+
+		request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request,
+				response);
 	}
 
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		ServletContext context = getServletContext( );
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		context.log("username: " + username + ", password: " + password);
+		
+		HttpSession s = request.getSession();
+		if (username.equals(password)) {
+			s.setAttribute("username", username);
+			s.setAttribute("user_role", "Viewer");
+			s.setAttribute("lggedIn", true);
+			
+			context.log("OK, redirect to home");
+			
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/jsp/home.jsp"); // login failed
+			dispatcher.forward(request, response);
+		} else {
+			context.log("NEIN NEIN NEIN!!!! login failed");
+			
+			s.setAttribute("lggedIn", false);
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/jsp/login.jsp"); // login failed
+			dispatcher.forward(request, response);
+		}
+	}
 }
--- jthr/jthr-parent/jthr-web/src/main/webapp/WEB-INF/jsp/login.jsp
+++ jthr/jthr-parent/jthr-web/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -3,36 +3,16 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>JTHR</title>
+<meta http-equiv="Content-Type"
+	content="text/html; charset=windows-1256">
+<title>JT HR</title>
 </head>
+
 <body>
-	<p>
-	<form action="LoginServlet" method="POST">
-		<table border="0">
-			<tr>
-				<td><label>Username:</label></td>
-				<td><input type="text" name="username"></td>
-			</tr>
-
-			<tr>
-				<td><label>Password:</label></td>
-				<td><input type="password" name="password"></td>
-			</tr>
-
-			<tr>
-				<td colspan="2"><input type="submit" value="Login"
-					name="submit"> <a href="register"><input type="button"
-						value="Register"></a></td>
-			</tr>
-
-			<tr>
-				<td colspan="2"><p style="color: red;">${error}</p></td>
-			</tr>
-
-		</table>
+	<form action="login" method="POST">
+	Username <input type="text" name="username" /><br>
+	Password <input type="password" name="password" /><br> 
+	<input type="submit" value="Login">
 	</form>
-	</p>
-
 </body>
 </html>
