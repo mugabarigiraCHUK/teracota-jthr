7d526926649ac2c285c0a0eeccca1b8fc4eeb94f
Ciolocoiu Catalin <Ciolocoiu.Catalin@gmail.com>
2014-09-19 17:51:07 +0300

    Tabelul este acuma dinamic !!! si tuti

--- jthr/jthr-parent/jthr-web/src/main/java/edu/msg/jthr/web/user/UserServlet.java
+++ jthr/jthr-parent/jthr-web/src/main/java/edu/msg/jthr/web/user/UserServlet.java
@@ -1,6 +1,8 @@
 package edu.msg.jthr.web.user;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,6 +29,13 @@ public class UserServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		ArrayList<User> list = new ArrayList<User>();
+		list.add(new User("unu", "unu", "1", "1", "unu@unu.com", "BOSS", "mergeee"));
+		list.add(new User("doi", "doi", "2", "2", "doi@doi.com", "BOSS2", "mergeee"));
+		list.add(new User("trei", "trei", "3", "3", "trei@trei.com", "BOSS3", "mergeee"));
+		list.add(new User("ptru", "ptru", "4", "4", "ptru@ptru.com", "BOSS4", "mergeee"));
+		list.add(new User("cinci", "cinci", "5", "5", "cinci@cinci.com", "BOSS5", "mergeee"));
+		request.getSession().setAttribute("list", list);
 		request.getRequestDispatcher("/WEB-INF/jsp/user.jsp").forward(request,response);
 		
 	}
