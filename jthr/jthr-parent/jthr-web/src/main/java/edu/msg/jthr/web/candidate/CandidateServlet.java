b5ba3a9bbdf36c3a61a9b85c52e56c20201ad738
Razvan <br.razvan@gmail.com>
2014-09-20 12:52:08 +0300

    addCandidate servlet and jsp done

--- jthr/jthr-parent/jthr-web/src/main/java/edu/msg/jthr/web/candidate/CandidateServlet.java
+++ jthr/jthr-parent/jthr-web/src/main/java/edu/msg/jthr/web/candidate/CandidateServlet.java
@@ -13,27 +13,34 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/CandidateServlet")
 public class CandidateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CandidateServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/jsp/candidate.jsp").forward(request,response);
+	public CandidateServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/jsp/candidate.jsp").forward(
+				request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		response.sendRedirect(request.getContextPath() + "/candidate");
 	}
 
 }
--- jthr/jthr-parent/jthr-web/src/main/java/edu/msg/jthr/web/position/AddPositionServlet.java
+++ jthr/jthr-parent/jthr-web/src/main/java/edu/msg/jthr/web/position/AddPositionServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class AddCandidateServlet
  */
-@WebServlet("/AddCandidateServlet")
+@WebServlet("/AddPositionServlet")
 public class AddPositionServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
