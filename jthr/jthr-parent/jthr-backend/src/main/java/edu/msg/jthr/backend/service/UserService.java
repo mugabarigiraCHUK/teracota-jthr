dbbf953c252c39bf312d9f691916f24d145a17c2
Ciolocoiu Catalin <Ciolocoiu.Catalin@gmail.com>
2014-09-25 15:53:58 +0300

    Instructiuni de la ADAM !!!

--- jthr/jthr-parent/jthr-backend/src/main/java/edu/msg/jthr/backend/service/UserService.java
+++ jthr/jthr-parent/jthr-backend/src/main/java/edu/msg/jthr/backend/service/UserService.java
@@ -6,11 +6,11 @@ import edu.msg.jthr.backend.model.User;
 
 public interface UserService {
 	
-	public void addUser(User user);
-	public void editUser(User user);
-	public void deleteUser(Long id);
-	public User getUserById(Long id);
-	public List<User> getAllUsers();
+	public void add(User user);
+	public void edit(User user);
+	public void delete(Long id);
+	public User getById(Long id);
+	public List<User> getAll();
 	
 
 }
