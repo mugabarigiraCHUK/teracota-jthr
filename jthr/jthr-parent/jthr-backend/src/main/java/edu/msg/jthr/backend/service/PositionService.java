1557097c44828fcc5bf2f3d2def3e5ddf9b06e51
gazsia <gazsia@gmail.com>
2014-09-25 19:38:08 +0300

    Created position service interface Created position service implementation

--- jthr/jthr-parent/jthr-backend/src/main/java/edu/msg/jthr/backend/service/PositionService.java
+++ jthr/jthr-parent/jthr-backend/src/main/java/edu/msg/jthr/backend/service/PositionService.java
@@ -1,5 +1,13 @@
 package edu.msg.jthr.backend.service;
 
+import java.util.List;
+
+import edu.msg.jthr.backend.model.Position;
+
 public interface PositionService {
-	
+	public void add(Position user);
+	public void edit(Position user);
+	public void delete(Long id);
+	public Position getById(Long id);
+	public List<Position> getAll();
 }
