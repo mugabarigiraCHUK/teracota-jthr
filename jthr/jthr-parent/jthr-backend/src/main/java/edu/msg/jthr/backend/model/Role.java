b8ff957f580b80c659e4e8e7ab6dd96159f19a72
Ciolocoiu Catalin <Ciolocoiu.Catalin@gmail.com>
2014-09-25 15:24:50 +0300

    Role , User and Department entityes created ! Heil Teracota !

---
+++ jthr/jthr-parent/jthr-backend/src/main/java/edu/msg/jthr/backend/model/Role.java
@@ -0,0 +1,33 @@
+package edu.msg.jthr.backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class Role extends BaseEntity{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Column
+	private String roleName;
+	
+	public Role() {}
+
+	public Role(String roleName) {
+		super();
+		this.roleName = roleName;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+	
+	
+}
