a61caf679d22b2f91af7b5cca09ffbb14574171c
Ciolocoiu Catalin <Ciolocoiu.Catalin@gmail.com>
2014-09-25 15:50:37 +0300

    --SERVICE + UserService
    --SERVICE IMPL + UserServiceImpl

--- jthr/jthr-parent/jthr-backend/src/main/java/edu/msg/jthr/backend/service/UserService.java
+++ jthr/jthr-parent/jthr-backend/src/main/java/edu/msg/jthr/backend/service/UserService.java
@@ -1,15 +1,16 @@
-package edu.msg.jthr.backend.repository;
+package edu.msg.jthr.backend.service;
 
 import java.util.List;
 
 import edu.msg.jthr.backend.model.User;
 
-public interface UserDAO {
+public interface UserService {
 	
 	public void addUser(User user);
 	public void editUser(User user);
 	public void deleteUser(Long id);
-	public List<User> getAllUsers();
 	public User getUserById(Long id);
+	public List<User> getAllUsers();
 	
+
 }
