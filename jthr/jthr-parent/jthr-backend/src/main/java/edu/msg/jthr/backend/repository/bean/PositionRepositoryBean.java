f94c02139478dc191563e0d4d355f9bd24f390f5
Razvan <br.razvan@gmail.com>
2014-09-26 18:50:10 +0300

    cccc

--- jthr/jthr-parent/jthr-backend/src/main/java/edu/msg/jthr/backend/repository/bean/PositionRepositoryBean.java
+++ jthr/jthr-parent/jthr-backend/src/main/java/edu/msg/jthr/backend/repository/bean/PositionRepositoryBean.java
@@ -5,19 +5,18 @@ import java.util.List;
 import javax.ejb.Stateless;
 
 import edu.msg.jthr.backend.model.Position;
-import edu.msg.jthr.backend.model.User;
 import edu.msg.jthr.backend.repository.PositionRepository;
 
-@Stateless (name="UserRepository", mappedName="ejb/UserRepository")
-public class PositionRepositoryBean extends BaseRepositoryBean<Position, Long> implements PositionRepository {
+@Stateless(name = "PositionRepository", mappedName = "ejb/PositionRepository")
+public class PositionRepositoryBean extends BaseRepositoryBean<Position, Long>
+		implements PositionRepository {
 
-	public PositionRepositoryBean(Class<Position> clazz) {
-		super(clazz);
-		// TODO Auto-generated constructor stub
+	public PositionRepositoryBean() {
+		super(Position.class);
 	}
 
 	@Override
-	public List<User> getPositionbyFilter(String pattern) {
+	public List<Position> getPositionbyFilter(String pattern) {
 		// TODO Auto-generated method stub
 		return null;
 	}
